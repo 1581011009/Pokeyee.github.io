3bc2617246b2216463287226b27b3c10e4f4006d
1581011009 <85081743+1581011009@users.noreply.github.com>
2021-06-01 05:32:06 +0800

    二次修改

--- 期末.js
+++ 期末.js
@@ -1,5 +1,17 @@
-            function topFunction() 
-            {
-                document.body.scrollTop = 0;
-                document.documentElement.scrollTop = 0;
-            }
+function topFunction() 
+{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function ShowTime()
+{
+　document.getElementById('time').innerHTML = new Date();
+　setTimeout('ShowTime()',1000);
+}
+
+function ChangeFontColor()
+{
+    var OriginalFont=document.getElementById("Font").innerHTML;
+    document.getElementById("Font").innerHTML='<font color="red">'+OriginalFont+'</font>';
+}
